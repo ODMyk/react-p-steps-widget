c7359b834197b3ad638ae504a8ca5edcc0b7eca2
Dmytro Ostapenko <ostapenko.dmytro05@gmail.com>
2024-07-06 22:12:00 +0300

    added dummydata

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import StepsWidget from './components/StepsWidget';
 import './main.scss'
 import { WalkingPeriod } from './types/WalkingPeriod';
+import { data as dummy } from './data/dummydata';
 
 function App() {
   const data: WalkingPeriod[] = JSON.parse(localStorage.getItem("steps_data") ?? "[]");
+  if (data.length === 0) {
+    dummy.map(p => data.push(p));
+    localStorage.setItem('steps_data', JSON.stringify(data));
+  }
 
   return (
     <>
